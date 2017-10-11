50e8257216362b31e9af95443ef70f60aaa85dfa
Maksim Chemerisuk <chemerisuk@gmail.com>
2017-10-11 10:29:16 +0300

    promisify plugin

--- www/FirebaseAnalytics.js
+++ www/FirebaseAnalytics.js
@@ -2,19 +2,29 @@ var exec = require("cordova/exec");
 var PLUGIN_NAME = "FirebaseAnalytics";
 
 module.exports = {
-    logEvent: function(name, params, success, error) {
-        exec(success, error, PLUGIN_NAME, "logEvent", [name, params || {}]);
+    logEvent: function(name, params) {
+        return new Promise(function(resolve, reject) {
+            exec(resolve, reject, PLUGIN_NAME, "logEvent", [name, params || {}]);
+        });
     },
-    setUserId: function(userId, success, error) {
-        exec(success, error, PLUGIN_NAME, "setUserId", [userId]);
+    setUserId: function(userId) {
+        return new Promise(function(resolve, reject) {
+            exec(resolve, reject, PLUGIN_NAME, "setUserId", [userId]);
+        });
     },
-    setUserProperty: function(name, value, success, error) {
-        exec(success, error, PLUGIN_NAME, "setUserProperty", [name, value]);
+    setUserProperty: function(name, value) {
+        return new Promise(function(resolve, reject) {
+            exec(resolve, reject, PLUGIN_NAME, "setUserProperty", [name, value]);
+        });
     },
-    setEnabled: function(enabled, success, error) {
-        exec(success, error, PLUGIN_NAME, "setEnabled", [enabled]);
+    setEnabled: function(enabled) {
+        return new Promise(function(resolve, reject) {
+            exec(resolve, reject, PLUGIN_NAME, "setEnabled", [enabled]);
+        });
     },
-    setCurrentScreen: function(name, success, error) {
-        exec(success, error, PLUGIN_NAME, "setCurrentScreen", [name]);
+    setCurrentScreen: function(name) {
+        return new Promise(function(resolve, reject) {
+            exec(resolve, reject, PLUGIN_NAME, "setCurrentScreen", [name]);
+        });
     }
 };
